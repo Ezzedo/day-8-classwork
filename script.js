4913eb83eba4692e6f7ac18ab22ddd11f282b98e
Ezzedo <zedozedoezz@gmail.com>
2025-08-22 14:25:09 +0300

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = prompt("Enter a name:");
+let age = 55;
+
+function checkAge(age){
+if (age >= 18){
+    console.log("You can drive");
+}
+
+else if(age < 18){
+    console.log("You're too young to drive"); 
+}
+}
+alert(`Hello ${name}, ${message}`);
